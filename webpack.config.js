b782ba597a9edf5082fda69248d6ba14f8341c14
Sergey Bachurin <ba4urin.sergey2016@yandex.ru>
2022-11-10 22:03:46 +0500

    Заменил табуляцию на пробелы

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,26 @@
 let mode = 'development';
 let hmr = true;
 if (process.env.NODE_ENV === 'production') {
-	mode = 'production';
-	
-	/* Убираем HMR в продакшене */
-	hmr = false;
+    mode = 'production';
+
+    /* Убираем HMR в продакшене */
+    hmr = false;
 }
 console.log(mode + ' mode');
 console.log('Hot Module Replacement ' + hmr);
 
 
 module.exports = {
-	mode: mode,
-	optimization: {
-		minimize: false,
-	},
-	
-	/* Настройки webpack-dev-server */
-	devServer: {
-		static: './dist',
-		hot: hmr
-	}
+    mode: mode,
+    optimization: {
+        minimize: false,
+    },
+
+    /* Настройки webpack-dev-server */
+    devServer: {
+        static: './dist',
+        hot: hmr
+    }
 };
 
 
